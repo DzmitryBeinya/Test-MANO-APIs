b19881c8a3c97eccbce58a5b1712c9e31846cc70
dzmitrybeinia <dzmitry.beinia@netcracker.com>
2018-09-17 11:18:30 +0300

    init

---
+++ Test-MANO-APIs/src/main/java/com/netcracker/Testmanoapis/TestManoApisApplication.java
@@ -0,0 +1,12 @@
+package com.netcracker.Testmanoapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestManoApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestManoApisApplication.class, args);
+	}
+}
